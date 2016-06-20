dc400de175451605e2f99aa2f338dbf87f0a50f7
Java1-2014024010 <pdzzo@nate.com>
2016-06-20 12:46:38 +0900

    calculator
    f

--- src/hello/cc.java
+++ src/hello/cc.java
@@ -3,13 +3,15 @@ package hello;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class cc extends JFrame {
+public class cc extends JFrame implements ActionListener {
 	private JPanel panel;
 	private JTextField display;
 	private JButton[] buttons;
@@ -37,7 +39,7 @@ public class cc extends JFrame {
 					buttons[index].setForeground(Color.CYAN);
 				buttons[index].setBackground(Color.LIGHT_GRAY);
 				panel.add(buttons[index]);
-				// buttons[index].addActionListener(this);
+				buttons[index].addActionListener(this);
 				index++;
 			}
 		}
@@ -47,6 +49,59 @@ public class cc extends JFrame {
 		setVisible(true);
 		pack();
 	}
+	
+	public void actionPerformed(ActionEvent e){
+	      String command = e.getActionCommand();
+	      if(command.charAt(0)=='C'){
+	         startOfNumber = true;
+	         result = 0;
+	         operator = "=";
+	         display.setText("0.0");
+	      } else if(command.charAt(0)>= '0' && command.charAt(0)<='9' && !command.equals("1/x") 
+	            || command.equals(".")){
+	         if(startOfNumber == true)
+	            display.setText(command);
+	         else
+	            display.setText(display.getText()+command);
+	         startOfNumber = false;
+	      }else {
+	         if (startOfNumber){
+	            if(command.equals("-")){
+	               display.setText(command);
+	               startOfNumber = false;
+	            }else
+	               operator = command;
+	            
+	         } else {
+	            double x = Double.parseDouble(display.getText());
+	            calculate(x);
+	            operator = command;
+	            startOfNumber = true;
+	         }
+	      }
+	   }
+	   
+	   private void calculate(double n) {
+	      if (operator.equals("+"))
+	         result += n;
+	      else if (operator.equals("-"))
+	         result -= n;
+	      else if (operator.equals("*"))
+	         result *= n;
+	      else if (operator.equals("/"))
+	         result /= n;
+	      else if (operator.equals("sqrt"))
+	         result = Math.sqrt(n);
+	      else if (operator.equals("1/x"))
+	         result = n/1;
+	      else if (operator.equals("%"))
+	         result = (int) result % n;
+	      else if (operator.equals("-/+"))
+	         result = (-1)*n;
+	      else if (operator.equals("="))
+	         result = n;
+	      display.setText(" " + result);
+	   }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
